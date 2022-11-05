2a44e2277a592c9731682da54146daebb5386705
vikash jha <vikashjha@vikashs-MacBook-Air.local>
2022-11-05 11:20:41 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import LoginScreen from './screens/LoginScreen';
+
+const App = () => {
+  return (
+   <View>
+    <LoginScreen/>
+   </View>
+  );
+};
+
+export default App;
