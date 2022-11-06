eecedb0a66c52018afdd88eb3e2f90ccf6885042
abhayambule <aambule3@gmail.com>
2022-11-06 23:07:06 +0530

    otp verification done

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,13 +1,13 @@
 import {View, Text, Button, TextInput} from 'react-native';
 import React, {useEffect, useState} from 'react';
-import UserInput from '../compoments/UserInput';
-import CustomButton from '../compoments/CustomButton';
 import Mobile from '../components/Mobile';
 import auth from '@react-native-firebase/auth';
+import TestMyPrep from './TestMyPrep';
 
 const LoginScreen = () => {
   const [confirm, setConfirm] = useState(null);
   const [value, setValue] = useState('');
+  const [authenticated, setAutheticated] = useState(false);
 
   const [code, setCode] = useState('');
 
@@ -22,17 +22,27 @@ const LoginScreen = () => {
 
   const signInWithPhoneNumber = async phoneNumber => {
     const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
-    console.log(confirmation);
+    // console.log(confirmation);
     setConfirm(confirmation);
   };
 
   const confirmCode = async () => {
     try {
       await confirm.confirm(code);
+      setConfirm(null);
     } catch (error) {
       console.log('Invalid code.');
     }
   };
+
+  auth().onAuthStateChanged(user => {
+    if (user) {
+      setAutheticated(true);
+    } else {
+      setAutheticated(false);
+    }
+  });
+  if (authenticated) return <TestMyPrep />;
 
   if (!confirm) {
     return (
@@ -54,14 +64,25 @@ const LoginScreen = () => {
     <>
       <View
         style={{
-          backgroundColor: 'red',
+          display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
         }}>
+        <Text style={{marginTop: 10, fontSize: 14}}>
+          code is sent to {value}
+        </Text>
         <TextInput
-          style={{borderWidth: 2, borderColor: 'red'}}
+          style={{
+            borderWidth: 0.5,
+            borderColor: 'black',
+            marginBottom: 20,
+            marginTop: 30,
+            width: 200,
+          }}
           value={code}
           onChangeText={text => setCode(text)}
+          maxLength={6}
+          keyboardType={'number-pad'}
         />
         <Button title="Confirm Code" onPress={() => confirmCode()} />
       </View>
